bfc6ac095d3e48b1b3ca26c0fb3981652d1a25f8
Magistra <magistraagis@gmail.com>
2023-10-24 14:37:36 +0800

    add conditional

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import Drawer from './Drawer';
+import Account from './Account';
+import LoginSignin from './LoginSignin';
 
 export default function Navbar() {
+  const isLogin = false
   return (
-    <div className='flex justify-center p-6 shadow-md'>
+    <div className='flex justify-center p-6 shadow-md sticky top-0 bg-base-100'>
       <div className='w-full flex justify-between items-center '>
         <div className='flex w-2/12  items-center'>
-          <Drawer />
-          <h2 className='text-2xl text-blue-500 font-bold'>Burung biru</h2>
+          <h2 className='text-2xl text-primary-content font-bold'>
+            Keluh-kesah <br /> mahasiswa
+          </h2>
         </div>
-        <input type="search" placeholder='Search here...' className='w-4/12 rounded-lg py-2 px-4 bg-transparent border border-purple-600' />
-        <div className='w-[200px] justify-between flex'>
-          <button className='btn btn-primary'>Log in</button>
-          <button className='btn btn-default'>Sign Up</button>
-        </div>
+        <input type='search' placeholder='Search here...' className='w-4/12 rounded-lg py-2 px-4 bg-transparent border border-primary-content' />
+        {
+          isLogin ? <Account/> : <LoginSignin/>
+        }
       </div>
     </div>
   );
