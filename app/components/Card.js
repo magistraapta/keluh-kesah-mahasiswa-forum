f8e27b298eb161b96df592b54f94f3432c2de6ce
Magistra <magistraagis@gmail.com>
2023-10-24 14:38:03 +0800

    add body

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function Card({title}) {
+export default function Card({title, body}) {
   return (
     <div className='p-4 bg-neutral shadow-lg rounded-lg mb-5'>
         <h2 className='text-2xl'>{title}</h2>
+        <div className='mt-4'>
+          <p>{body}</p>
+        </div>
     </div>
   )
 }
