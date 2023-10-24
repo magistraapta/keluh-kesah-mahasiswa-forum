7867e04aec6c35292889123590ea68b8bdaad04d
Magistra <magistraagis@gmail.com>
2023-10-24 14:37:46 +0800

    add footer

---
+++ app/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='bg-primary p-4 sticky bottom-0 shadow-md'>
+        <p className='text-white text-xs'>magistraapta | 2023</p>
+    </div>
+  )
+}
