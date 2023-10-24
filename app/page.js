710d381dfeb4eb2c4b113ffb4793da0cde58fe01
Magistra <magistraagis@gmail.com>
2023-10-24 14:38:15 +0800

    add footer

--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@ import Image from 'next/image';
 import Navbar from './components/Navbar';
 import 'material-icons/iconfont/material-icons.css';
 import Content from './components/Content';
+import Footer from './components/Footer';
 export default function Home() {
   return (
     <main className=''>
       <Navbar />
       <Content/>
+      <Footer/>
     </main>
   );
 }
