1427940de9aad74db181abedc53f960aa15d3420
Magistra <magistraagis@gmail.com>
2023-10-25 15:27:34 +0800

    add login page

---
+++ app/login/page.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from 'next/image';
+import LoginPict from '../assets/login-pict.jpeg'
+import LoginForm from './components/LoginForm';
+export default function Login() {
+  return (
+    <div className='flex justify-center  items-center h-screen'>
+      <div className='p-8  w-11/12 h-5/6 bg-primary text-white flex rounded-md'>
+        <div className='w-6/12 '>
+          <h1 className='font-bold text-5xl leading-tight'>Join us in the biggest  student community platform</h1>
+          <LoginForm/>
+        </div>
+        <div className=' h-full w-6/12  items-center flex'>
+            <Image alt='login-pict' src={LoginPict} className='rounded-lg h-full'/>
+        </div>
+      </div>
+    </div>
+  );
+}
